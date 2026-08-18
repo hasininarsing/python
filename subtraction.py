e9191dd9b5f17c6c2567f3fc38afa1bdcb4af050
hasininarsing <hasininarsing@gmail.com>
2026-08-18 11:31:55 +0530

    Create subtraction.py

---
+++ subtraction.py
@@ -0,0 +1,9 @@
+# Step 1: Request inputs from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Step 2: Calculate the difference
+result = num1 - num2
+
+# Step 3: Display the output
+print(f"The result of subtracting {num2} from {num1} is: {result}")
